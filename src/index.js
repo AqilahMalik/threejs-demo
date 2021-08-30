7db1cec7fc93aed32cdf876b14d2847b7d3aeb4e
AqilahMalik <aqilahsimaa98@gmail.com>
2021-08-30 17:14:10 +0800

    Added lots of cubes

--- src/index.js
+++ src/index.js
@@ -107,13 +107,37 @@ light.position.z = 10;
 
 sphere.position.x = 15;
 
+let cubes = [];
+let cubeCount = 500;
+
+for (let i = 1; i <= cubeCount; i += 1) {
+  let c = createCube();
+  c.position.x = Math.random() * 400 - 200; // -200 to 200, random 3D position
+  c.position.y = Math.random() * 400 - 200;
+  c.position.z = Math.random() * 400 - 200;
+  cubes.push(c);
+}
+
+console.log(cubes.length);
+
 scene.add(axesHelper);
-scene.add(cube, sphere, light, lightHelper);
+scene.add(cube, sphere, light, lightHelper, ...cubes);
+// ... = separate operator, (run the fx multiple time)
 
 // anytime camera n scene chnage or run, we need to rerender(renderer fx used)
 renderer.render(scene, camera);
 
 function animate() {
+  cube.rotation.x += 0.05;
+  cube.rotation.y += 0.05;
+  cube.rotation.z += 0.05;
+
+  cubes.forEach(function (c) {
+    c.rotation.x += -0.03;
+    c.rotation.y += -0.03;
+    c.rotation.z += -0.03;
+  });
+
   //  Inrement and decrement
   // cube.position.x += 0.1;
   // cube.position.y -= 0.1;
